656df0dee7192247b2463459983dbcb0d4442b56
WilliamCui31 <444738577@qq.com>
2018-07-15 23:26:08 +0800

    修改数据格式

--- utils.js
+++ utils.js
@@ -1,12 +1,10 @@
-
-const dayTime = 1000 * 60 * 60 * 24 //1天；
+const dayTime = 1000 * 60 * 60 * 24; //1天；
 const hourTime = 1000 * 60 * 60;
 const minuteTime = 1000 * 60;
 const secondTime = 1000;
 const vnum = 2554477;
 
 const setRandomValue = (integerNum, decimalNum) => {
-
   let randomNum = Math.random() * Math.pow(10, integerNum) + Math.pow(10, integerNum);
   // 整数部分
   let integerPart = 0;
@@ -14,66 +12,56 @@ const setRandomValue = (integerNum, decimalNum) => {
     integerPart = Math.floor(randomNum);
   }
   //  小数部分
-  let decimalPart = 0.00;
+  let decimalPart = 0.0;
   if (decimalNum) {
-    decimalPart = randomNum.toString().split(".")[1].substring(0, decimalNum);
+    decimalPart = randomNum
+      .toString()
+      .split('.')[1]
+      .substring(0, decimalNum);
     decimalPart = '0.' + decimalPart;
     decimalPart = parseFloat(decimalPart);
   }
   return integerPart + decimalPart;
-}
+};
 /**
- * 
- * @param {*长度} length 
- * @param {*从第几位开始} startLength 
+ *
+ * @param {*长度} length
+ * @param {*从第几位开始} startLength
  */
-let setRandomData = (params) => {
-
+let setRandomData = params => {
   let { step, length, type, increasingNum, startTime, endTime } = params;
-  let t = [];
-  let c = [];
-  let o = [];
-  let h = [];
-  let l = [];
-  let v = [];
-  let s = "ok";
-  let num = "";
-  let time = ""
+  let num = '';
+  let time = '';
+  const klineData = [];
   for (let i = 0; i < length; i++) {
     num = setRandomValue(2, 2);
-    h.push(num);
-    l.push(num - setRandomValue(1));
-    o.push(num - setRandomValue(1));
-    c.push(num - setRandomValue(1));
-    v.push(vnum - setRandomValue(6));
 
-    if (type === "front") {
+    if (type === 'front') {
       time = Math.floor((endTime - step * (i + 1)) / 1000);
-      t.unshift(time);
-    } else if (type === "back") {
+    } else if (type === 'back') {
       time = Math.floor((endTime + step * (increasingNum + i + 1)) / 1000);
-      t.push(time);
     }
 
+    klineData.push({
+      t: time,
+      h: num,
+      s: 'ok',
+      c: num - setRandomValue(1),
+      o: num - setRandomValue(1),
+      l: num - setRandomValue(1),
+      v: vnum - setRandomValue(6)
+    });
   }
 
-  return {
-    t,
-    c,
-    o,
-    h,
-    l,
-    v,
-    s
-  }
-}
+  return klineData;
+};
 
-const filteringTime = (time) => {
+const filteringTime = time => {
   let minuteTime = 60;
   let dayTime = 60 * 60 * 24;
   let longTime = 0;
   switch (time) {
-    case "1D":
+    case '1D':
       longTime = dayTime * 1;
       break;
     default:
@@ -81,36 +69,34 @@ const filteringTime = (time) => {
       break;
   }
   return longTime;
-}
+};
 
-
-const transformTime = (time) => {
+const transformTime = time => {
   let number = 0;
   if (time.indexOf('min') !== -1) {
     number = parseInt(time.split('min')[0]);
-    return number * minuteTime
+    return number * minuteTime;
   }
 
   if (time.indexOf('hour') !== -1) {
     number = parseInt(time.split('hour')[0]);
-    return number * hourTime
+    return number * hourTime;
   }
 
   if (time.indexOf('D') !== -1) {
     number = parseInt(time.split('D')[0]);
-    return number * dayTime
+    return number * dayTime;
   }
 
   if (time.indexOf('W') !== -1) {
     number = parseInt(time.split('W')[0]);
-    return number * dayTime * 7
+    return number * dayTime * 7;
   }
-}
-
+};
 
 module.exports = {
   setRandomValue,
   setRandomData,
   filteringTime,
   transformTime
-}
+};
--- wsOperation.js
+++ wsOperation.js
@@ -1,5 +1,3 @@
-
-
 const utils = require('./utils');
 const config = require('./config');
 const defaultTime = new Date().getTime();
@@ -7,55 +5,52 @@ const defaultTime = new Date().getTime();
 let Timer = null;
 
 const dealMessage = (message, ws) => {
-  let data = JSON.parse(message);
-  let { type, period, from, to, baseCurrencyId, targetCurrencyId } = data;
-  // 控制每次添加的时间数
-  let step = utils.transformTime(period) / config.constant.SPACE_TIME;
-  // console.log('step ->', step);
-
-  if (type === "kline") {
-    console.log('kline......')
+  if (message.indexOf('_') !== -1) {
+    const from = new Date('2018-06-14').getTime();
+    const to = new Date().getTime();
+    // 控制每次添加的时间数
+    let step = utils.transformTime('1min') / config.constant.SPACE_TIME;
+    // console.log('step ->', step);
     const params = {
       step,
       length: config.constant.HISTORY_DATA_LENGTH,
-      type: "front",
+      type: 'front',
       startTime: from,
       endTime: to
-    }
+    };
 
     let result = utils.setRandomData(params);
     // console.log('from->', from);
     // console.log('to->', to);
-    let returnData = {}
-    let compareTime = defaultTime - 1 * step
+    let returnData = {};
+    let compareTime = defaultTime - 1 * step;
     let compareDay = new Date(compareTime);
 
     // console.log('compareDay->', compareDay);
-    console.log('period->', period);
+    // console.log('period->', period);
     console.log('step->', step);
     if (compareDay >= from) {
       returnData = {
-        "code": 0,
-        "type": "kline",
-        "data": {
-          ...result
-        }
-      }
-
+        code: 0,
+        type: 'kline',
+        data: result
+      };
     } else {
       returnData = {
-        "code": 0,
-        "type": "kline",
-        "data": {
-          t: [],
-          c: [],
-          o: [],
-          h: [],
-          l: [],
-          v: [],
-          s: "ok"
-        }
-      }
+        code: 0,
+        type: 'kline',
+        data: [
+          {
+            t: '',
+            c: '',
+            o: '',
+            h: '',
+            l: '',
+            v: '',
+            s: 'ok'
+          }
+        ]
+      };
     }
 
     // console.log('send kline');
@@ -68,56 +63,45 @@ const dealMessage = (message, ws) => {
     }
     console.log('建立定时器...');
     Timer = startRealtime(ws, params);
+  } else {
+    ws.send('pong');
   }
-}
-
+};
 
 // 定时发送实时数据
 const startRealtime = (ws, params) => {
   // console.log('startRealtime ->');
   let { step, length, type, startTime, endTime } = params;
   let increasingNum = 0;
-  let data = {}
-
+  let data = {};
 
   return setInterval(() => {
     increasingNum = ++increasingNum;
-    let returnData = {}
+    let returnData = {};
     const _params = {
       step,
       length: 1,
-      type: "back",
+      type: 'back',
       increasingNum,
       startTime,
       endTime
-    }
+    };
 
     result = utils.setRandomData(_params);
     console.log('result ->', result);
     let { t, c, o, h, l, v } = result;
     returnData = {
-      "code": 0,
-      "type": "dealSuccess",
-      "data": {
-        kLine: {
-          t: t[0],
-          c: c[0],
-          o: o[0],
-          h: h[0],
-          l: l[0],
-          v: v[0],
-          s: "ok"
-        }
-      }
+      code: 0,
+      type: 'realTime',
+      data: [result[0]]
+    };
+
+    if (ws.readyState === 1) {
+      ws.send(JSON.stringify(returnData));
     }
-    ws.send(JSON.stringify(returnData));
-  }, config.constant.INTERVAL_TIME)
-}
-
-
+  }, config.constant.INTERVAL_TIME);
+};
 
 module.exports = {
-  dealMessage,
-
-}
-
+  dealMessage
+};
